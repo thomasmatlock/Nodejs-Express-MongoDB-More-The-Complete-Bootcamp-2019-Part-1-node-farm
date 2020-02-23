0dae2d4690f125b051aa935c6156a52e5d320e6e
Tom <metaphorps@gmail.com>
2020-02-22 20:38:53 -0600

    fun stuff

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+// BLOCKING CODE (SYNCHRONOUS)
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// const textOut = `Hello, we the this about it: ${textIn} \nCreated on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log("File written.");
 
-const textOut = `Hello, we the this about it: ${textIn} \nCreated on ${Date.now()}`;
-
-fs.writeFileSync('./txt/output.txt', textOut);
-console.log('File written.');
+// NON BLOCKING CODE (ASYNC)
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+	// async here, arg1 filepath, arg2 cb function to run on completion. file encoding type is unneeded but doing it just cuz
+	// cb func args = error and actual data
+	fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+		console.log(data2);
+		fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
+			console.log(data3);
+		});
+	});
+});
+console.log('Will read file');
